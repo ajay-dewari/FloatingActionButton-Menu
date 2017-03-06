12fe57086b2daa28d9e8d6d819630653cadaa190
Ajay <ajdewari@gmail.com>
2017-03-07 01:17:56 +0530

    New Behavior added, if snackbar come in any case the FAB will also scrool up

---
+++ app/src/main/java/com/ajaysinghdewari/floatingactionbuttonmenu/activities/utils/MoveUpwardBehavior.java
@@ -0,0 +1,39 @@
+package com.ajaysinghdewari.floatingactionbuttonmenu.activities.utils;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Ajay on 07-03-2017.
+ */
+
+public class MoveUpwardBehavior extends CoordinatorLayout.Behavior<View> {
+    public MoveUpwardBehavior() {
+        super();
+    }
+
+    public MoveUpwardBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+        return dependency instanceof Snackbar.SnackbarLayout;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
+        float translationY = Math.min(0, ViewCompat.getTranslationY(dependency) - dependency.getHeight());
+        ViewCompat.setTranslationY(child, translationY);
+        return true;
+    }
+
+    //you need this when you swipe the snackbar(thanx to ubuntudroid's comment)
+    public void onDependentViewRemoved(CoordinatorLayout parent, View child, View dependency) {
+        ViewCompat.animate(child).translationY(0).start();
+    }
+}
